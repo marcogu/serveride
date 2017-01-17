dbb3aa80e1273d60db3022b8b8b99b3b38b83585
marco <penpenqie@qq.com>
2017-01-17 12:54:02 +0800

    1 fix mac cac auto complation byte error

--- app/mobels/reqarg/package.scala
+++ app/mobels/reqarg/package.scala
@@ -14,9 +14,6 @@ package object reqarg {
   )
 
   object MockMkQuery{
-    def queryMainKey(psam:String)(implicit db:Map[String, String] = Map.empty[String, String]):String = db(psam) match {
-      case null =>  "9D37F2AF79A3B42F9D37F2AF79A3B42F"
-      case other => other
-    }
+    def queryMainKey(psam:String)(implicit db:Map[String, String]) = "9D37F2AF79A3B42F9D37F2AF79A3B42F"
   }
 }
