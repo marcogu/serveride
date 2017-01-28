3468557843d42fa8ce3dcdc4f5daf460343cd734
marco <penpenqie@qq.com>
2017-01-28 11:46:56 +0800

    develop

--- app/assets/javascripts/editor/devapp.js
+++ app/assets/javascripts/editor/devapp.js
@@ -37,9 +37,6 @@ app.controller('EditorController', function ($scope,$http) {
             var editor = CodeMirror.fromTextArea(ele, {
                 lineNumbers: true,
                 matchBrackets: true,
-                height:'800px',
-                // width:'100%',
-                // theme: "ambiance",
                 mode: "text/x-scala"
             });
         });
